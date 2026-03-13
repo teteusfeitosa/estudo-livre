4088669fc4c8eb829467448170af72280ad03eaf
Matheus Feitosa <teteusmatheus801@gmail.com>
2026-03-13 08:49:11 -0300

    ajeitando executáveis

--- exercicios/lista01/Questao02.java
+++ exercicios/lista01/Questao02.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 import exercicios.Questao;
 /**
- * Calcule o salário mínimo, salário do cidadão referido e atribua quantas salários ele ganha unitariamente
+ * Calcule o salário mínimo, salário do cidadão referido e atribua quantas salários ele  ganha unitariamente.
+ * 
  */
 public class Questao02 extends Questao {
 
     public void processar(){
   
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("qual valor do salário mínimo no brasil? ");
-        double minimo = scanner.nextDouble();
+        double minimo = sc.nextDouble();
 
         System.out.print("qual valor do seu salário? ");
-        double salario = scanner.nextDouble();
+        double salario = sc.nextDouble();
         
         System.out.println("quantos salários mínimos vocẽ recebe? " + (salario / minimo));
-
-        scanner.close();
+        sc.close();
     } 
     
 }
